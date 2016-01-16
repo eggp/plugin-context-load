5897ea77af8ea63752c6e13f771bf6f0a5630f4c
eggp <eggprojec@gmail.com>
2016-01-16 12:03:00 +0100

    edit version number

--- plugin-context-load.php
+++ plugin-context-load.php
@@ -4,7 +4,7 @@
 Plugin Name: Plugin Context Load
 Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
 Description: A brief description of the Plugin.
-Version: 1.0
+Version: 0.1
 Author: eggp
 Author URI: http://URI_Of_The_Plugin_Author
 License: A "Slug" license name e.g. GPL2
